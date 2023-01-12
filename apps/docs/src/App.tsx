8c79fb295695353dcf2d1911d679cf77e0267c4f
NUANCE\Shrihari.Dani <shrihari.dani@nuance.com>
2023-01-12 17:05:30 +0530

    fourth commit

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <h1 className="header">
           Docs
-          <div className="Turborepo">Turborepo Example docs</div>
+          <div className="Turborepo">Turborepo Example docs!!</div>
         </h1>
         <div>
           <Link className="App-link" href="https://turbo.build/repo">
